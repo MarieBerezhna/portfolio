60c094afcb9307954632d833f49b3b82b6517814
weirdox <marie.berezhna@gmail.com>
2020-09-10 02:04:58 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <MainMenu :sections="sections" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from './components/MainMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MainMenu
+  },
+  data () {
+    return {
+      sections: [
+        {
+          name: 'Home'
+        },
+        {
+          name: 'Skills'
+        },
+        {
+          name: 'Projects'
+        },
+        {
+          name: 'Contact'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: rgb(32, 23, 23);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #aaa;
 }
 </style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <nav>
+            <a href="/">
+                <img :src="require('../assets/logo.png')" alt="logo" class="logo">
+            </a>
+            <ul>
+                <li v-for="sec in sections" :key="sec.name" class="nav-link">
+                    <a :href="'#' + sec.name.toLowerCase()">{{ sec.name.toUpperCase() }}</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainMenu',
+    props: {
+        sections: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+    width: 200px;
+    height: 100px;
+    position: fixed;
+    top: 10px;
+    left: 10px;
+}
+nav {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0,0,0,0.9);
+        ul {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        list-style: none;
+        li, a {
+            margin: 1% 10px;
+            float: right;
+            color: #fff;
+            text-decoration: none;
+            font-family: "AvantGarde Bold", Sans-serif;
+            font-weight: 800;
+            width: 100px;
+            min-width: 100px;
+        }
+    }
+}
+
+</style>

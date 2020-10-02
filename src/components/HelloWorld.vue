18b47d7a08e98d9f8e459fcf23c40de175ad9ad8
weirdox <marie.berezhna@gmail.com>
2020-10-03 01:29:00 +0300

    'boxes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,12 +22,12 @@
 
           <div v-for="box in this.$store.state.boxes" :key="box.id" class="infobox-outer col-12 col-md-4 my-5 my-md-1">
             <div class="infobox text-center mb-5" @mouseenter="boxHover" @mouseleave="boxHover"
-              :class="{highlighted: box.highlighted}" >
+              :class="{highlighted: box.highlighted}">
               <div class="img mx-auto">
                 <img class="img w-25 my-3 my-md-4" :src="require('../assets/png/' + box.icon)" :alt="box.title">
               </div>
               <h2>{{ box.title }} </h2>
-              <hr class="bg-dark w-50">
+              <hr class="w-50" :class="{'bg-dark' : box.highlighted}">
               <p class=" pb-5 px-2  text-left"> {{ box.content }} </p>
             </div>
           </div>
@@ -50,10 +50,12 @@
         let primary = this.$store.state.primary_color
         let secondary = this.$store.state.secondary_color
         let item = e.target
+        let hr = item.getElementsByTagName('hr')[0]
         if (!item.classList.contains('highlighted')) {
           item.style.backgroundColor = e.type === 'mouseenter' ? primary : secondary
           item.style.color = e.type === 'mouseenter' ? secondary : '#fff'
-          item.getElementsByTagName('hr')[0].style.visibility = e.type === 'mouseenter' ? "visible" : "hidden"
+         hr.style.backgroundColor = e.type === 'mouseenter' ? "#343a40" : '#fff' 
+
         }
       }
     },
@@ -63,16 +65,14 @@
     mounted() {
       let greenBox = document.getElementsByClassName('highlighted')[0];
       greenBox.style.backgroundColor = this.$store.state.primary_color
-      greenBox.style.color =this.$store.state.secondary_color
-      greenBox.getElementsByTagName('hr')[0].style.visibility = "visible"
-
+      greenBox.style.color = this.$store.state.secondary_color
+      
       let colorCss = `
       <style>
-.infobox-outer:last-child .infobox::before {
-    background-color: ` + this.color + `;
-}
-</style>
-      `
+      .infobox-outer:last-child .infobox::before {
+          background-color: ` + this.$store.state.primary_color + `;
+      }
+      </style>`
       document.getElementById('homestyle').innerHTML = colorCss
     }
   }
@@ -83,7 +83,6 @@
   #profile-image {
     padding-left: 0;
     height: 100vh;
-    // min-height: 500px;
     width: 100%;
     background-image: url(../assets/grey.jpg);
     background-size: 120%;
@@ -110,14 +109,14 @@
       cursor: pointer;
 
       hr {
-        visibility: hidden;
         height: 5px;
+        background-color: #fff;
       }
     }
 
     &:last-child .infobox {
       &:hover {
-        box-shadow: 0px 10px 50px 10px rgba(0,0,0,0.3);
+        box-shadow: 0px 10px 50px 10px rgba(0, 0, 0, 0.3);
       }
 
       &::before {
@@ -135,7 +134,7 @@
   @media (min-width: 992px) {
     #profile-image {
       background-size: 90%;
-    background-position: left;
+      background-position: left;
     }
 
     .greet {

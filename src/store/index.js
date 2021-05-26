a79544526b059cedd8f5084e02e14f1c7b68fc90
weirdox <marie.berezhna@gmail.com>
2021-05-26 18:10:26 +0300

    projects

--- src/store/index.js
+++ src/store/index.js
@@ -33,9 +33,9 @@ const state = {
   primary_color: '#5BFFAA',
   secondary_color: '#3B3B3B',
   sections: [{
-    id: 0,
-    name: 'Home'
-  },
+      id: 0,
+      name: 'Home'
+    },
     {
       id: 1,
       name: 'Skills'
@@ -50,15 +50,15 @@ const state = {
     }
   ],
   boxes: [{
-    id: 0,
-    title: 'Creative Mindset',
-    icon: 'puzzle.png',
-    content: `2+ years of experience in web development with a strong focus on front-end web applications,
+      id: 0,
+      title: 'Creative Mindset',
+      icon: 'puzzle.png',
+      content: `2+ years of experience in web development with a strong focus on front-end web applications,
           striving for responsive websites and user-friendly interfaces.
           Trying out different styles and creating my own, 
           as a naturally wondering creature, I like to play with things and see what happens.`,
-    highlighted: true
-  },
+      highlighted: true
+    },
     {
       id: 1,
       title: 'Forever Student',
@@ -83,10 +83,10 @@ const state = {
   ],
   coreskills: {
     markup_and_layout: [{
-      id: 0,
-      name: 'HTML',
-      icon: ''
-    },
+        id: 0,
+        name: 'HTML',
+        icon: ''
+      },
       {
         id: 1,
         name: 'CSS',
@@ -104,9 +104,9 @@ const state = {
       }
     ],
     client_and_server_side: [{
-      id: 0,
-      name: 'JS'
-    },
+        id: 0,
+        name: 'JS'
+      },
       {
         id: 1,
         name: 'jQuery',
@@ -124,10 +124,10 @@ const state = {
       }
     ],
     vcs_and_data_bases: [{
-      id: 0,
-      name: 'Git',
-      icon: ''
-    },
+        id: 0,
+        name: 'Git',
+        icon: ''
+      },
       {
         id: 1,
         name: 'GitHub',
@@ -146,11 +146,11 @@ const state = {
     ]
   },
   extraskills: [{
-    id: 0,
-    text: `Competent at working with design prototypes made in Figma and AdobeXD and
+      id: 0,
+      text: `Competent at working with design prototypes made in Figma and AdobeXD and
               capable of getting along with other design tools. 
               Have an eye for good design and following best practicies.`
-  },
+    },
     {
       id: 1,
       text: `Knowledgeable about mobile-first development strategy, SEO, 
@@ -169,9 +169,9 @@ const state = {
     }
   ],
   social_icons: [{
-    name: 'facebook-f',
-    url: 'https://www.facebook.com/WebMarieCreative'
-  },
+      name: 'facebook-f',
+      url: 'https://www.facebook.com/WebMarieCreative'
+    },
     {
       name: 'linkedin-in',
       url: 'https://www.linkedin.com/in/webmarie/'
@@ -190,13 +190,13 @@ const state = {
     }
   ],
   projects: [{
-    id: 0,
-    name: 'Eli`s Pub',
-    url: '/elispub/',
-    img: 'elispub.jpg',
-    description: 'Local Live Music Pub',
-    tags: []
-  },
+      id: 0,
+      name: 'Eli`s Pub',
+      url: '/elispub/',
+      img: 'elispub.jpg',
+      description: 'Local Live Music Pub',
+      tags: []
+    },
     {
       id: 1,
       name: 'Tomatick',
@@ -237,16 +237,33 @@ const state = {
         Landing page for an HR campaign.`,
       tags: []
     },
+    {
+      id: 6,
+      name: "Friends",
+      url: "https://friends.allstars-it.com/",
+      img: 'friends.jpg',
+      description: `Fullstack project (Node.js, Vue). 
+        Landing page for an HR campaign.`,
+      tags: []
+    },
+    {
+      id: 7,
+      name: "Tech Speaker",
+      url: "https://techspeaker.allstars-it.com/",
+      img: "allstars.jpg",
+      description: "",
+      tags: []
+    }
   ],
   footerlinks: [{
-    id: 0,
-    name: 'Terms of use & Privacy',
-    code: 'tos',
-    content: `The only information I collect on this webpage is your name
+      id: 0,
+      name: 'Terms of use & Privacy',
+      code: 'tos',
+      content: `The only information I collect on this webpage is your name
            and your email, when you make an enquiery with a contact form. 
           The only use of that information for me is to get back to you with answers, 
           I don't pass it to any third parties.`
-  },
+    },
     {
       id: 1,
       name: 'Credits',
@@ -260,7 +277,7 @@ const state = {
 };
 const actions = {
   contact() {
-    
+
   }
 };
 export default new Vuex.Store({

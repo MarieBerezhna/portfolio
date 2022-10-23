31fa0a85e0195383fbd022b69fb7b95401d19186
marie <marie.berezhna@gmail.com>
2022-10-23 16:12:36 +0200

    remove dots from projects section

--- src/components/SectionProjects.vue
+++ src/components/SectionProjects.vue
@@ -20,23 +20,12 @@
                     </div>
                 </div>
             </div>
-            <div class="container">
-                <div class="dots-wrapper row position-relative">
-                    <div class="col text-center mt-3">
-
-                        <span v-for="project in projects" :key="project.id">
-                            <span :class="{active: project.id === projects.length -1 }" :item="project.id" class="dot"
-                                @click="dotClick($event)"></span>
-                        </span>
-                    </div>
-                </div>
-            </div>
         </section>
     </div>
 </template>
 
 <script>
-    import $ from 'jquery'
+    // import $ from 'jquery'
     export default {
         name: 'SeactionProjects',
         components: {
@@ -80,67 +69,6 @@
                 ],
             }
         },
-        methods: {
-            setStyles() {
-                const num = $('.project').length
-                const width = $('.project').width()
-                const vm = this
-
-                // set offscreen wrapper width
-                $('.projects-wrapper').width(num * width)
-                // add a dot for each offscreen item
-                $('.project').each(function (el) {
-                    let visible = vm.checkVisibleX($('.project'), el)
-                    let spare = $('.dot')[$('.dot').length - 1]
-                    if (visible && el > 0) {
-                        $(spare).parent().remove()
-                    } else {
-                        let item = $('.project')[el]
-                        $(item).addClass('init-hidden')
-                    }
-                })
-                let styles = `<style>
-                .active {
-                    background: ` + this.$store.state.primary_color + `;
-                }
-                .dot {
-                    border: 1px solid ` + this.$store.state.primary_color + `
-                } </style>`
-                $('#projects').prepend(styles)
-            },
-            dotClick(e) {
-                const current = $('.dot.active');
-                const cIdx = $('.dot.active').attr('item')
-                const tIdx = $(e.target).attr('item')
-                let dist = $('.project').width()
-                dist = dist * Math.abs(tIdx - cIdx)
-                let direction = tIdx > cIdx ? '-=' : '+='
-                $('.project').animate({
-                    left: direction + dist
-                },100)
-                $(current).removeClass('active')
-                $(e.target).addClass('active')
-            },
-            activeDots: function () { //bug
-                $('.container-fluid').on('scroll', function () {
-                    $('.project').each((el) => {
-                        let item =$('.project')[el]
-                        let winSide = window.innerWidth - $(item).width() / 2
-                        let itemSide = $(item).position().left   
-                        if ( itemSide >= 0 && itemSide < winSide) {
-                            let dot = $('.dot[item="'+  $(item).attr('id') +'"]')
-                            $('.dot.active').removeClass('active')
-                            $(dot).addClass('active')
-                        }
-                    })
-                })
-            }
-        },
-        mounted() {
-            this.setStyles()
-            this.activeDots()
-            $($('.dot')[0]).addClass('active')
-        }
     }
 </script>
 
@@ -154,23 +82,6 @@
         cursor: pointer;
     }
 
-    .container-fluid {
-        height: 100vw;
-        display: -webkit-box;
-        overflow-y: hidden;
-        overflow-x: auto;
-    }
-
-    .projects-wrapper {
-        display: -webkit-box;
-        overflow-y: hidden;
-        overflow-x: auto;
-    }
-
-    .projects-wrapper::-webkit-scrollbar,
-    .container-fluid::-webkit-scrollbar {
-        display: none;
-    }
     .project {
         padding: 0 !important;
         height: 100vw;
@@ -241,12 +152,9 @@
     }
 
     @media (min-width: 992px) {
-        .container-fluid {
-            height: 33vw;
-        }
 
         .project {
-            width: 33.33vw;
+            width: 33vw;
             height: 33vw;
             &:hover .content .p-heading {
                 font-size: 3rem !important;
